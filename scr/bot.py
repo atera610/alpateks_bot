1313482afe9d0df9eed3dba4336cc25ded1d530e
atera610 <atera.610@gmail.com>
2022-01-17 20:20:53 +0300

    expenses and income

---
+++ scr/bot.py
@@ -0,0 +1,23 @@
+from dotenv import dotenv_values
+from telegram.ext import Updater, MessageHandler, Filters, CommandHandler
+import logging
+from dialogprocessor import DialogProcessor
+
+
+config = dotenv_values('../.env')
+tg_token = config['TELEGRAM_BOT_TOKEN']
+tg_chat_id = config['TELEGRAM_CHAT_ID']
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+
+
+if __name__ == '__main__':
+    updater = Updater(tg_token)
+    dispatcher = updater.dispatcher
+
+    dialog_processor = DialogProcessor(chat_id=tg_chat_id)
+    dispatcher.add_handler(CommandHandler("start", dialog_processor.start))
+    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, dialog_processor.process_message))
+
+    updater.start_polling()
+    updater.idle()
